d342314d72120383874de823dee087cd0661a20e
dindo-techs-it <esmanid0718@gmail.com>
2026-02-26 20:46:30 +0900

    codes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { AppProvider } from './context/AppContext';
 import Dashboard from './components/Dashboard';
+import Alerts from './components/Alerts'; // <--- Don't forget this import!
 import './index.css';
 
 function App() {
   return (
     <AppProvider>
       <div className="app-container">
+        {/* Alerts usually go at the top so they are visible immediately */}
+        <Alerts /> 
         <Dashboard />
-        <Alerts />
       </div>
     </AppProvider>
   );
